954ffda7206c257ac4a972856308429a02e9d67c
zongpingzhang <zongpingzhang@zongpingzhangdemac.local>
2019-01-14 22:06:07 -0800

    Initial Commit

---
+++ AllHeart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllHeart
+//
+//  Created by zongpingzhang on 2019/1/14.
+//  Copyright © 2019年 zongpingzhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
